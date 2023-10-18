3764cbedf359bd3eec1d59a08cc990103f84760a
deepplus <antonrepo81@gmail.com>
2023-10-18 18:37:14 +0300

    complete of responsive of main hero

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -23,9 +23,9 @@ const Menu = () => {
             {name}
           </h1>
         </Link>
-        <ul className="hidden sm:flex">
+        <ul className="hidden md:flex">
           {navigation.map((item) => (
-            <li className='px-4' key={item.name}>
+            <li className='px-4 flex items-center' key={item.name}>
               <Link
                 spy={true}
                 active="active"
@@ -39,10 +39,17 @@ const Menu = () => {
               </Link>
             </li>
           ))}
+          <li className='px-4'>
+            <button
+              className={`bg-color-green rounded-full text-white px-5 py-2 shine shadow-md`}
+            >
+              お部屋を送る
+            </button>
+          </li>
         </ul>
 
         {/* Mobile Button */}
-        <div onClick={handleNav} className="block sm:hidden z-10">
+        <div onClick={handleNav} className="block md:hidden z-10">
           {nav ? (
             <AiOutlineClose size={20} className={`text-white`} />
           ) : (
@@ -51,7 +58,7 @@ const Menu = () => {
         </div>
         {/* Mobile Menu */}
         <div
-          className={"sm:hidden absolute left-0 right-0 bottom-0 justify-center items-center w-full h-screen bg-color-primary text-center ease-in duration-500 pb-10 " + (nav ? "top-0 overflow-auto" : "top-[-100vh] overflow-hidden")}
+          className={"md:hidden absolute left-0 right-0 bottom-0 justify-center items-center w-full h-screen bg-color-primary text-center ease-in duration-500 pb-10 " + (nav ? "top-0 overflow-auto" : "top-[-100vh] overflow-hidden")}
         >
           <div className={`text-left text-white px-5 py-5 text-2xl`}>{name}</div>
           <ul className={`text-white`}>
@@ -76,15 +83,15 @@ const Menu = () => {
                 <button className={`bg-color-green font-bold text-2xl rounded-full shine shadow-md px-6 py-3`}>{mainHero.sendBtnTitle}</button>
               </a>
             </li>
-            <li className={`items-center px-7 justify-center text-black`}>
-              <div className={`text-xl px-8 py-4 shadow-md rounded-lg bg-color-skin flex flex-col gap-y-5`}>
-                <div className={`text-left flex`}>
-                  <div className='w-2/5'>対応時間</div>
-                  <div className='w-3/5 font-bold'>10:00〜19:00</div>
+            <li className={`items-center px-7 justify-center text-black md:`}>
+              <div className={`text-xl px-8 py-4 shadow-md rounded-lg bg-color-skin flex flex-col gap-y-5 w-full`}>
+                <div className={`flex sm:gap-x-12 gap-x-3`}>
+                  <div className='text-right w-2/5'>対応時間</div>
+                  <div className='text-left w-3/5 font-bold'>10:00〜19:00</div>
                 </div>
-                <div className={`text-left flex`}>
-                  <div className='w-2/5'>営業日</div>
-                  <div className='w-3/5 font-bold'>水曜定休</div>
+                <div className={`flex sm:gap-x-12 gap-x-3`}>
+                  <div className='text-right w-2/5'>営業日</div>
+                  <div className='text-left w-3/5 font-bold'>水曜定休</div>
                 </div>
               </div>
             </li>
